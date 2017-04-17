69e4016b1fa188ab272bc195d0b0cffd10991a1e
jone <jone.liu@etocrm.com>
2017-04-17 15:02:20 +0800

    添加安装脚本

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if[	! -d ~/.local/bin ]
+if[ ! -d ~/.local/bin ]
 then
 	mkdir -p ~/.local/bin
 fi
@@ -15,6 +15,11 @@ then
 fi
 cp config/* ~/.config
 mv ~/.config/gitconfig ~/.gitconfig
+mv ~/.config/vimrc ~/.vimrc
+mkdir ~/.vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 pacman -S --noconfirm volumeicon scrot lxappearance compton feh conky\
 	xautolock xbacklight fcitx fcitx-im fcitx-sunpin
