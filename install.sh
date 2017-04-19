d633f33b2e7ed25823973c723e9274fd24eb483f
jone <jone.liu@etocrm.com>
2017-04-19 11:37:11 +0800

    feat: 添加Templates文件夹

--- install.sh
+++ install.sh
@@ -14,12 +14,15 @@ then
 	mkdir -p ~/.config
 fi
 cp config/* ~/.config
+
 mv ~/.config/gitconfig ~/.gitconfig
+
 mv ~/.config/vimrc ~/.vimrc
 mkdir ~/.vim
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mv Templates ~/
 
 pacman -S --noconfirm volumeicon scrot lxappearance compton feh conky\
 	xautolock xbacklight fcitx fcitx-im fcitx-sunpin sakura rofi volumeicon
